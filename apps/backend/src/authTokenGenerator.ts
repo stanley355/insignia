1ea004d8900e1097a91f3826952c10ddfdcc8c5c
Stanley Winata <winatastanley355@gmail.com>
2023-04-06 15:05:45 +0700

    Token module

--- apps/backend/src/authTokenGenerator.ts
+++
@@ -1,8 +0,0 @@
-import * as jwt from 'jsonwebtoken';
-
-export class AuthTokenGenerator {
-  generateAuthToken(payload: any): string {
-    const token = jwt.sign(payload, 'secret');
-    return token;
-  }
-}
