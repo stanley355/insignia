f3076118685f55dd9b29a9969129d942fc2eaca8
Stanley Winata <winatastanley355@gmail.com>
2023-04-06 14:55:11 +0700

    reset

--- apps/backend/src/workspace/workspace.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { WorkspaceService } from './workspace.service';
-
-@Module({
-  providers: [WorkspaceService],
-  controllers: [],
-})
-export class WorkspaceModule {}
